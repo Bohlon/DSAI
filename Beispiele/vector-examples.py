5008006c3ea2bb622af7067f649cf6bb64e9f385
David Miklauc <deif.mida@gmail.com>
2024-02-28 15:27:46 +0100

    added examples for vectors

---
+++ Beispiele/vector-examples.py
@@ -0,0 +1,13 @@
+from typing import List
+Vector = List[float]
+
+height_weight_age = [170, # cm
+                     87, # kg
+                     40, # age
+                     ]
+
+grades = [95, # exam1
+          80, # exam2
+          75, # exam3
+          62, # exam4
+          ]
